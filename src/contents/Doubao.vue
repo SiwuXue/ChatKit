87fc67b6a26308b9b832fc05174659f77d4f1a99
SiwuXue <3488909960@qq.com>
2026-03-06 12:40:53 +0800

    init project

---
+++ src/contents/Doubao.vue
@@ -0,0 +1,39 @@
+<script lang="ts">
+import type {
+    PlasmoCSConfig,
+    PlasmoGetInlineAnchor,
+    PlasmoMountShadowHost
+} from "plasmo"
+
+export const config: PlasmoCSConfig = {
+    matches: ["https://www.doubao.com/*"]
+}
+
+const getInlineAnchor: PlasmoGetInlineAnchor = () =>
+    document.querySelector(".flex-nowrap")
+
+const mountShadowHost: PlasmoMountShadowHost = ({ anchor, shadowHost }) => {
+    anchor!.element!.insertBefore(shadowHost!, anchor!.element!.firstChild)
+}
+
+export default {
+    plasmo: {
+        getInlineAnchor,
+        mountShadowHost
+    },
+    data() {
+        return {
+            count: 0
+        }
+    },
+    setup() { },
+    mounted() { }
+}
+</script>
+
+<template>
+    <div>
+        <span style="color: red">{{ count }}</span>
+        <button @click="count++">Many Myths are based on truth</button>
+    </div>
+</template>
---
+++ src/options.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+
+<template>
+    <div>
+        <h1>Options</h1>
+    </div>
+</template>
+<style scoped>
+h1 {
+    color: #470;
+}
+</style>
